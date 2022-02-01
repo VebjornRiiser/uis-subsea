d7ffb2820c7427f9987b456f1e90837ef8214d45
VebjornRiiser <12122688+VebjornRiiser@users.noreply.github.com>
2022-02-01 11:00:34 +0100

    Made network connection robust, with error handling and restarting connections

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from Subsea_QT_GUI import GUI_loop
-import controller, nettverkskommunikasjon, requests, threading, os, socket, json
+import controller
+import threading
+import socket
 from multiprocessing import Pipe, Process
-
 from Subsea_QT_GUI import *
 
-def controllerdata_to_json(controll_data):
-    pass   
-    can = {}
-    # return json.dumps()
 
-# socket.create_connection()
+def controllerdata_to_json(controll_data):
+    pass
+
 
 def run_webapp(connection):
     pass
 
+
 def send_data_to_rov():
     pass
 
+
 def receive_data_from_rov():
     pass
+
 
 def recieve_commands_from_webapp(host="127.0.0.1", port=9999):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -36,7 +38,7 @@ def recieve_commands_from_webapp(host="127.0.0.1", port=9999):
 
 
 def relay_data_from_controller(connection_controller, relay=True):
-    thread = threading.current_thread()
+    # thread = threading.current_thread()
     # while getattr(thread, "run", True):
     while True:
         controller_data = connection_controller.recv()
@@ -48,10 +50,6 @@ def relay_data_from_controller(connection_controller, relay=True):
         if relay:
             pass
 
-
-
-def send_data_to_rov():
-    pass
 
 if __name__ == "__main__":
     print("starting")
@@ -65,10 +63,9 @@ if __name__ == "__main__":
 
     gui_loop = Process(target=GUI_loop.run, args=(gui_child_pipe,))
     gui_loop.start()
-    
 
     recv_frm_cnt = threading.Thread(target=relay_data_from_controller, daemon=True, args=(parent_conn, True))
     recv_frm_cnt.start()
-    
+
     recv_frm_web_app = threading.Thread(target=recieve_commands_from_webapp)
     recv_frm_web_app.start()
--- network_handler.py
+++ network_handler.py
@@ -1,4 +1,15 @@
-import threading, socket, time
+import threading
+import socket
+import sys
+import time
+import json
+
+global local
+local = False
+if len(sys.argv) > 1:
+    if sys.argv[1] == "True":
+        local = True
+
 
 class Network:
 
@@ -7,13 +18,13 @@ class Network:
         self.bind_addr = bind_addr
         self.connect_addr = connect_addr
         self.port = port
-        
+        self.waiting_for_conn = True
         self.conn = None
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.timeout = -1
 
         self.new_conn()
-        self.heartbeat = threading.thread(target=self.beat)
-
+        self.heartbeat = threading.Thread(target=self.beat)
+        self.heartbeat.start()
 
     def beat(self):
         while True:
@@ -21,36 +32,101 @@ class Network:
             time.sleep(0.3)
 
     def new_conn(self):
+        self.conn = None
+        self.waiting_for_conn = True
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
         if self.is_server:
-            wait_for_conn = threading.thread(target=self.wait_for_conn)
+            print(f"trying to bind with {self.bind_addr, self.port}")
+            self.socket.bind((self.bind_addr, self.port))
+            self.socket.listen()
+            print("starting wait for conn")
+            wait_for_conn = threading.Thread(target=self.wait_for_conn)
             wait_for_conn.start()
         else:
-            self.socket.connect((self.connect_addr, self.port))
+            print("Client is trying to connect")
+            while True:
+                try:
+                    self.socket.connect((self.connect_addr, self.port))
+                except ConnectionRefusedError:
+                    print("Connection refused. Trying again in 500 ms")
+                    time.sleep(0.5)
+                    continue
+                else: # only do this if we do not get an error
+                    self.waiting_for_conn = False
+                    break
+            self.conn = self.socket
+            print(f"connection from client has been established. conn: {self.conn}")
+            # print(self.socket.gettimeout())
+            self.socket.settimeout(0.4)
 
     def wait_for_conn(self) -> None:
         while True:
+            print("Server is waiting on connection")
             self.conn, addr = self.socket.accept()
-            print(f"New connection from {addr}")
+            self.waiting_for_conn = False
 
+            print(f"New connection from {addr}. conn: {self.conn}, addr")
 
     def send(self, bytes_to_send: bytes) -> None:
-        if self.conn is None and self.is_server == False:
-            raise("Client tried sending with a non existing connection")
+        if self.conn is None and not self.waiting_for_conn:
+            raise Exception("Client tried sending with a non existing connection")
+        # print(f"waiting for conn: {self.waiting_for_conn}")
+        if self.waiting_for_conn:
+            print("waiting for conn")
+            return
         try:
+            if type(bytes_to_send) != bytes:
+                print(f"tried sending something that was not bytes: {bytes_to_send} type: {type(bytes_to_send)}")
+                exit(1)
             self.conn.sendall(bytes_to_send)
-        except Exception:
-            self.new_conn()
+        except socket.error as e: # need to define the actual exceptions we should handle
+            print(f"line (56) Tried sending, but got an error \n{e}")
+            print(f"conn = {self.conn}, waiting for conn: {self.waiting_for_conn}")
+            if not self.waiting_for_conn:
+                self.new_conn()
 
     def receive(self) -> bytes:
-        return self.conn.recv()
+        # print(self.conn)
+        if self.conn is None:
+            if not self.waiting_for_conn:
+                self.wait_for_conn()
+            return
+        try:
+            return self.conn.recv(1024)
+        except socket.error as e:
+            print(f"line 68 Exception: {e}")
 
+    def exit(self):
+        self.conn.close()
 
+# Do not get error here if
 def send_forever(conn):
+    print("sending forever")
+    data = {"test": 2, "abc": [1,2,34]}
+    data = bytes(json.dumps(data), "utf-8")
     while True:
-        
+        conn.send(data)
+
+def recieve_forever():
+    pass
 
 if __name__ == "__main__":
-    client_conn = Network()
-    server_conn = Network(is_server=True)
-
-    threading.Thread(target = lambda: send_forever(client_conn))
+    print(sys.platform)
+if sys.platform != "win32":
+    server_conn = Network(is_server=True, bind_addr="0.0.0.0")
+    # server_conn = Network(is_server=True, bind_addr="127.0.0.1")
+    # threading.Thread(target=recieve_forever).start()
+    while True:
+        data = server_conn.receive()
+        if not data:
+            continue
+        print(data)
+else:
+    client_conn = Network(connect_addr="10.0.0.2")
+    send_thread = threading.Thread(target=lambda: send_forever(client_conn))
+    send_thread.start()
+    send_thread.join()
+    while True:
+        time.sleep(1)
+    
