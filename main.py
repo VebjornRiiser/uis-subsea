d5dcda1837601ecc7731a8f33db380fffcf828c1
VebjornRiiser <12122688+VebjornRiiser@users.noreply.github.com>
2022-02-02 15:33:35 +0100

    Added support for stopping threads

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 from Subsea_QT_GUI import GUI_loop
-from multiprocessing import Pipe, Process
+from multiprocessing import Pipe, Process, connection
 from Subsea_QT_GUI import *
 import controller
 import threading
 import random
 import time
 import json
-
-def controllerdata_to_json(controll_data):
-    pass
+import sys
 
 
-def run_webapp(connection):
+global thread_should_run
+thread_should_run = []
+
+class Threadwatcher:
+    def __init__(self) -> None:
+        self.id = -1
+        self.threads = {}
+
+    def add_thread(self) -> int:
+        self.id += 1
+        self.threads[self.id] = True
+        return self.id
+
+    def should_run(self, id):
+        if id in self.threads:
+            return self.threads[id]
+        return True
+
+    def stop_thread(self, id):
+        self.threads[id] = False
+
+    def stop_all_threads(self):
+        for i in range(len(self.threads)):
+            self.stop_thread(id)
+
+def controllerdata_to_json(controll_data: dict) -> str:
+
+    return json.dumps(controll_data)
     pass
 
 
@@ -23,21 +48,23 @@ def receive_data_from_rov():
     pass
 
 
-def relay_data_from_controller(connection_controller, relay=True):
+def relay_data_from_controller(connection_controller, t_watch: Threadwatcher, id, relay=True):
     # thread = threading.current_thread()
     # while getattr(thread, "run", True):
-    while True:
+    while t_watch.should_run(id):
         controller_data = connection_controller.recv()
-        print(str(controller_data)[1:-1], end="    \r")
+        # print(str(controller_data)[1:-1], end="                                                      \r")
         # print(controller_data["joysticks"])
 
-        # sys.stdout.write(f"{controller_data}"+"\r")
-        # sys.stdout.flush()
+        sys.stdout.write(f"{controller_data}"+"         \r")
+        sys.stdout.flush()
         if relay:
+            # print(controllerdata_to_json(controller_data), end="            \r")
+            # print(controllerdata_to_json(controller_data))
             pass
 
-def recieve_commands_from_gui(conn):
-    while True:
+def recieve_commands_from_gui(conn, t_watch: Threadwatcher, id):
+    while t_watch.should_run:
         try:
             conn.recv()
         except KeyboardInterrupt:
@@ -54,9 +81,9 @@ def create_test_sensordata(delta, old_sensordata=None):
         sensordata["temp_vann"] = old_sensordata["temp_vann"] + 0.3*delta
     return sensordata
 
-def send_sensordata_to_gui(conn):
+def send_sensordata_to_gui(conn, t_watch: Threadwatcher, id):
     sensordata = create_test_sensordata(1)
-    while True:
+    while t_watch.should_run(id):
         delta = random.randint(-1,1)
         try:
             sensordata = create_test_sensordata(delta, sensordata)
@@ -64,32 +91,40 @@ def send_sensordata_to_gui(conn):
             conn.send(sensordata)
             time.sleep(0.2)
         except KeyboardInterrupt:
-            return
-
+            t_watch.stop_thread(id)
 
 if __name__ == "__main__":
     global start_time_sec
     start_time_sec = time.time()
     print("starting")
     # with open("config.txt", 'r') as config:
-        # print(config.read()[1])
+    #     print(config.read()[1])
+
+    t_watch = Threadwatcher()
 
     
+
+
+    id = t_watch.add_thread()
     parent_conn, child_conn = Pipe() # takes in controller data and sends it into child_conn
-    controller_process = Process(target=controller.run, args=(child_conn, True,))
+    controller_process = Process(target=controller.run, args=(child_conn, True, False, t_watch, id), daemon=True)
     controller_process.start()
 
+    id = t_watch.add_thread()
     gui_parent_pipe, gui_child_pipe = Pipe() # starts the gui program. gui_parent_pipe should get the sensor data
-    gui_loop = Process(target=GUI_loop.run, args=(gui_child_pipe,)) # and should recieve commands from the gui
-    # gui_loop.should_stop = False
+    gui_loop = Process(target=GUI_loop.run, args=(gui_child_pipe, t_watch, id)) # and should recieve commands from the gui
     gui_loop.start()
 
-    recv_from_gui = threading.Thread(target=lambda: recieve_commands_from_gui(gui_parent_pipe))
+    id = t_watch.add_thread()
+    recv_from_gui = threading.Thread(target=recieve_commands_from_gui, args=(gui_parent_pipe, t_watch, id),daemon=True)
     recv_from_gui.start()
 
-    send_to_gui = threading.Thread(target=lambda:send_sensordata_to_gui(gui_parent_pipe))
+    id = t_watch.add_thread()
+    send_to_gui = threading.Thread(target=send_sensordata_to_gui, args=(gui_parent_pipe, t_watch, id), daemon=True)
     send_to_gui.start()
 
-    recv_frm_cnt = threading.Thread(target=relay_data_from_controller, daemon=True, args=(parent_conn, True))
+    id = t_watch.add_thread()
+    recv_frm_cnt = threading.Thread(target=relay_data_from_controller, args=(parent_conn, t_watch, id, True), daemon=True)
     recv_frm_cnt.start()
-    controller_process.join()
+
+    gui_loop.join()
