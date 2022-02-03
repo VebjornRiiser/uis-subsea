fda1670e1785068d155f8ef2a5e41b7fbb3560a9
VebjornRiiser <12122688+VebjornRiiser@users.noreply.github.com>
2022-02-03 13:55:55 +0100

    Added threadwatch class file

---
+++ Threadwatch.py
@@ -0,0 +1,21 @@
+class Threadwatcher:
+    def __init__(self) -> None:
+        self.id = -1
+        self.threads = {}
+
+    def add_thread(self) -> int:
+        self.id += 1
+        self.threads[self.id] = True
+        return self.id
+
+    def should_run(self, id):
+        if id in self.threads:
+            return self.threads[id]
+        return True
+
+    def stop_thread(self, id):
+        self.threads[id] = False
+
+    def stop_all_threads(self):
+        for i in range(len(self.threads)):
+            self.stop_thread(id)
