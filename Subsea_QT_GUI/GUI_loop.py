4bf2a0a249ffb5e9d1920b5658b8213cf8ddb570
Åse Jortveit Sagebakken <aase.sagebakken@hotmail.com>
2022-02-22 11:38:30 +0100

    can move, resize window

--- Subsea_QT_GUI/GUI_loop.py
+++ Subsea_QT_GUI/GUI_loop.py
@@ -1,10 +1,10 @@
 from tkinter import Widget
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QLabel, QFileDialog
+from PyQt5 import QtCore, QtGui, QtWidgets, Qt
+from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QLabel, QFileDialog, QApplication, QWidget, QVBoxLayout, QSizeGrip, QFrame
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.Qt import *
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import QColor, QIcon, QCursor
 from PyQt5.QtWidgets import QSizeGrip
 from multiprocessing import Pipe, Value
 from Threadwatch import Threadwatcher
@@ -15,11 +15,15 @@ import threading
 import json
 import os
 import Subsea_QT_GUI.SUBSEAGUI as SUBSEAGUI
+import Subsea_QT_GUI.custom_grips as custom_grips
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QGraphicsDropShadowEffect, QPushButton, QApplication, QComboBox
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QtMsgType, QTimer, QEvent
 
+# GLOBALS
+# ///////////////////////////////////////////////////////////////
+GLOBAL_STATE = False
+GLOBAL_TITLE_BAR = True
 
-os.system('pyuic5 -x NYGUI.ui -o SUBSEAGUI.py')
 os.environ["QT_FONT_DPI"] = "96" # FIX Problem for High DPI and Scale above 100%
 
 class AnotherWindow(QWidget):
@@ -49,28 +53,54 @@ class Window(QMainWindow, SUBSEAGUI.Ui_MainWindow):
         self.t_watch = t_watch
         self.id = id
 
+        # Remove frame around window
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setup_gui_with_folder_change()
-        
 
-        self.manuell_btn.clicked.connect(self.button_test)
+        # Menu button clicked
         self.kontroller_btn.clicked.connect(lambda: self.change_current_widget(2))
         self.informasjon_btn.clicked.connect(lambda: self.change_current_widget(1))
 
+        # GUI button clicked
+        self.manuell_btn.clicked.connect(self.button_test)
 
         self.init_drop_shadow()
         self.connect_test_values()
+        
+        # ///////////////////////////////////////////////////////////////
+        # MOVE WINDOW
+        def moveWindow(event):
+            # IF MAXIMIZED CHANGE TO NORMAL
+            if self.returStatus():
+                self.maximize_restore()
+            # MOVE WINDOW
+            if event.buttons() == Qt.LeftButton:
+                self.move(self.pos() + event.globalPos() - self.dragPos)
+                self.dragPos = event.globalPos()
+                event.accept()
+        self.titleRightInfo.mouseMoveEvent = moveWindow
 
+        # CUSTOM GRIPS
+        self.left_grip = CustomGrip(self, Qt.LeftEdge, True)
+        self.right_grip = CustomGrip(self, Qt.RightEdge, True)
+        self.top_grip = CustomGrip(self, Qt.TopEdge, True)
+        self.bottom_grip = CustomGrip(self, Qt.BottomEdge, True)
 
         # RESIZE WINDOW
+        #vboxlayout = QVBoxLayout()
+        sizegrip = QSizeGrip(self)
+        sizegrip.setVisible(True)
+        #vboxlayout.addWidget(sizegrip)
+        #self.setLayout(vboxlayout)
+        
         self.sizegrip = QSizeGrip(self.frame_size_grip)
         self.sizegrip.setStyleSheet("width: 20px; height: 20px; margin 0px; padding: 0px;")
 
         # MINIMIZE
-        self.minimizeAppBtn.clicked.connect(lambda: self.showMinimized())
+        self.minimizeAppBtn.clicked.connect(self.minimize)
 
         # MAXIMIZE/RESTORE
-        self.maximizeRestoreAppBtn.clicked.connect(lambda: UIFunctions.maximize_restore(self))
+        self.maximizeRestoreAppBtn.clicked.connect(self.maximize_restore)
 
         # CLOSE APPLICATION
         # self.closeAppBtn.clicked.connect(lambda: self.close())
@@ -94,6 +124,54 @@ class Window(QMainWindow, SUBSEAGUI.Ui_MainWindow):
         self.comboBox_B_btn.addItems(["test", "test2"])
         self.comboBox_B_btn.setCurrentIndex(1)
 
+    # MOUSE CLICK EVENTS
+    # ///////////////////////////////////////////////////////////////
+    def mousePressEvent(self, event):
+        # SET DRAG POS WINDOW
+        self.dragPos = event.globalPos()
+
+        # PRINT MOUSE EVENTS
+        if event.buttons() == Qt.LeftButton:
+            print('Mouse click: LEFT CLICK')
+        if event.buttons() == Qt.RightButton:
+            print('Mouse click: RIGHT CLICK')
+
+    def resizeEvent(self, event):
+        # Update Size Grips
+        self.resize_grips()
+
+    def minimize(self):
+        print("clicked minimized")
+        self.showMinimized()
+        self.showNormal()
+
+
+    def maximize_restore(self):
+        global GLOBAL_STATE
+        status = GLOBAL_STATE
+        if status == False:
+            self.showMaximized()
+            GLOBAL_STATE = True
+            self.appMargins.setContentsMargins(0, 0, 0, 0)
+            self.maximizeRestoreAppBtn.setToolTip("Restore")
+            self.maximizeRestoreAppBtn.setIcon(QIcon(u":/icons/images/icons/icon_restore.png"))
+            self.frame_size_grip.hide()
+            self.left_grip.hide()
+            self.right_grip.hide()
+            self.top_grip.hide()
+            self.bottom_grip.hide()
+        else:
+            GLOBAL_STATE = False
+            self.showNormal()
+            self.resize(self.width()+1, self.height()+1)
+            self.appMargins.setContentsMargins(10, 10, 10, 10)
+            self.maximizeRestoreAppBtn.setToolTip("Maximize")
+            self.maximizeRestoreAppBtn.setIcon(QIcon(u":/icons/images/icons/icon_maximize.png"))
+            self.frame_size_grip.show()
+            self.left_grip.show()
+            self.right_grip.show()
+            self.top_grip.show()
+            self.bottom_grip.show()
 
     def shutdown(self):
         self.t_watch.stop_all_threads()
@@ -134,7 +212,6 @@ class Window(QMainWindow, SUBSEAGUI.Ui_MainWindow):
         print("recieved close thread. trying to close")
         # self.close()
         
-
     def button_test(self):
         # print("Clicked on button")
         self.w1.stream1.load(QtCore.QUrl("http://vg.no"))
@@ -155,7 +232,7 @@ class Window(QMainWindow, SUBSEAGUI.Ui_MainWindow):
 
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
-        self.setContentsMargins(0, 0, 0, 0)
+        self.appMargins.setContentsMargins(10, 10, 10, 10)
 
     ## SET VALUES TO DEF progressBarValue
     def setValue(self, slider, labelPercentage, progressBarName, color):
@@ -229,14 +306,253 @@ class Window(QMainWindow, SUBSEAGUI.Ui_MainWindow):
 
         self.lys_slider.valueChanged.connect(lambda: self.setValue(self.lys_slider, self.lys_percentage, self.lys, "rgba(85, 170, 255, 255)"))
 
+    def resize_grips(self):
+            ENABLE_CUSTOM_TITLE_BAR = True
+            self.left_grip.setGeometry(0, 10, 10, self.height())
+            self.right_grip.setGeometry(self.width() - 10, 10, 10, self.height())
+            self.top_grip.setGeometry(0, 0, self.width(), 10)
+            self.bottom_grip.setGeometry(0, self.height() - 10, self.width(), 10)
 
+    # RETURN STATUS
+    # ///////////////////////////////////////////////////////////////
+    def returStatus(self):
+        return GLOBAL_STATE
+
+    # SET STATUS
+    # ///////////////////////////////////////////////////////////////
+    def setStatus(self, status):
+        global GLOBAL_STATE
+        GLOBAL_STATE = status
+
+
+
+# CUSTOM WIDGET (LEGG I EGEN FIL)
+class CustomGrip(QWidget):
+    def __init__(self, parent, position, disable_color = False):
+
+        # SETUP UI
+        QWidget.__init__(self)
+        self.parent = parent
+        self.setParent(parent)
+        self.wi = Widgets()
+
+        # SHOW TOP GRIP
+        if position == Qt.TopEdge:
+            self.wi.top(self)
+            self.setGeometry(0, 0, self.parent.width(), 10)
+            self.setMaximumHeight(10)
+
+            # GRIPS
+            top_left = QSizeGrip(self.wi.top_left)
+            top_right = QSizeGrip(self.wi.top_right)
+
+            # RESIZE TOP
+            def resize_top(event):
+                delta = event.pos()
+                height = max(self.parent.minimumHeight(), self.parent.height() - delta.y())
+                geo = self.parent.geometry()
+                geo.setTop(geo.bottom() - height)
+                self.parent.setGeometry(geo)
+                event.accept()
+            self.wi.top.mouseMoveEvent = resize_top
+
+            # ENABLE COLOR
+            if disable_color:
+                self.wi.top_left.setStyleSheet("background: transparent")
+                self.wi.top_right.setStyleSheet("background: transparent")
+                self.wi.top.setStyleSheet("background: transparent")
+
+        # SHOW BOTTOM GRIP
+        elif position == Qt.BottomEdge:
+            self.wi.bottom(self)
+            self.setGeometry(0, self.parent.height() - 10, self.parent.width(), 10)
+            self.setMaximumHeight(10)
+
+            # GRIPS
+            self.bottom_left = QSizeGrip(self.wi.bottom_left)
+            self.bottom_right = QSizeGrip(self.wi.bottom_right)
+
+            # RESIZE BOTTOM
+            def resize_bottom(event):
+                delta = event.pos()
+                height = max(self.parent.minimumHeight(), self.parent.height() + delta.y())
+                self.parent.resize(self.parent.width(), height)
+                event.accept()
+            self.wi.bottom.mouseMoveEvent = resize_bottom
+
+            # ENABLE COLOR
+            if disable_color:
+                self.wi.bottom_left.setStyleSheet("background: transparent")
+                self.wi.bottom_right.setStyleSheet("background: transparent")
+                self.wi.bottom.setStyleSheet("background: transparent")
+
+        # SHOW LEFT GRIP
+        elif position == Qt.LeftEdge:
+            self.wi.left(self)
+            self.setGeometry(0, 10, 10, self.parent.height())
+            self.setMaximumWidth(10)
+
+            # RESIZE LEFT
+            def resize_left(event):
+                delta = event.pos()
+                width = max(self.parent.minimumWidth(), self.parent.width() - delta.x())
+                geo = self.parent.geometry()
+                geo.setLeft(geo.right() - width)
+                self.parent.setGeometry(geo)
+                event.accept()
+            self.wi.leftgrip.mouseMoveEvent = resize_left
+
+            # ENABLE COLOR
+            if disable_color:
+                self.wi.leftgrip.setStyleSheet("background: transparent")
+
+        # RESIZE RIGHT
+        elif position == Qt.RightEdge:
+            self.wi.right(self)
+            self.setGeometry(self.parent.width() - 10, 10, 10, self.parent.height())
+            self.setMaximumWidth(10)
+
+            def resize_right(event):
+                delta = event.pos()
+                width = max(self.parent.minimumWidth(), self.parent.width() + delta.x())
+                self.parent.resize(width, self.parent.height())
+                event.accept()
+            self.wi.rightgrip.mouseMoveEvent = resize_right
+
+            # ENABLE COLOR
+            if disable_color:
+                self.wi.rightgrip.setStyleSheet("background: transparent")
+    
+
+
+    def mouseReleaseEvent(self, event):
+        self.mousePos = None
+
+    def resizeEvent(self, event):
+        if hasattr(self.wi, 'container_top'):
+            self.wi.container_top.setGeometry(0, 0, self.width(), 10)
+
+        elif hasattr(self.wi, 'container_bottom'):
+            self.wi.container_bottom.setGeometry(0, 0, self.width(), 10)
+
+        elif hasattr(self.wi, 'leftgrip'):
+            self.wi.leftgrip.setGeometry(0, 0, 10, self.height() - 20)
+
+        elif hasattr(self.wi, 'rightgrip'):
+            self.wi.rightgrip.setGeometry(0, 0, 10, self.height() - 20)
+
+class Widgets(object):
+    def top(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        self.container_top = QFrame(Form)
+        self.container_top.setObjectName(u"container_top")
+        self.container_top.setGeometry(QRect(0, 0, 500, 10))
+        self.container_top.setMinimumSize(QSize(0, 10))
+        self.container_top.setMaximumSize(QSize(16777215, 10))
+        self.container_top.setFrameShape(QFrame.NoFrame)
+        self.container_top.setFrameShadow(QFrame.Raised)
+        self.top_layout = QHBoxLayout(self.container_top)
+        self.top_layout.setSpacing(0)
+        self.top_layout.setObjectName(u"top_layout")
+        self.top_layout.setContentsMargins(0, 0, 0, 0)
+        self.top_left = QFrame(self.container_top)
+        self.top_left.setObjectName(u"top_left")
+        self.top_left.setMinimumSize(QSize(10, 10))
+        self.top_left.setMaximumSize(QSize(10, 10))
+        self.top_left.setCursor(QCursor(Qt.SizeFDiagCursor))
+        self.top_left.setStyleSheet(u"background-color: rgb(33, 37, 43);")
+        self.top_left.setFrameShape(QFrame.NoFrame)
+        self.top_left.setFrameShadow(QFrame.Raised)
+        self.top_layout.addWidget(self.top_left)
+        self.top = QFrame(self.container_top)
+        self.top.setObjectName(u"top")
+        self.top.setCursor(QCursor(Qt.SizeVerCursor))
+        self.top.setStyleSheet(u"background-color: rgb(85, 255, 255);")
+        self.top.setFrameShape(QFrame.NoFrame)
+        self.top.setFrameShadow(QFrame.Raised)
+        self.top_layout.addWidget(self.top)
+        self.top_right = QFrame(self.container_top)
+        self.top_right.setObjectName(u"top_right")
+        self.top_right.setMinimumSize(QSize(10, 10))
+        self.top_right.setMaximumSize(QSize(10, 10))
+        self.top_right.setCursor(QCursor(Qt.SizeBDiagCursor))
+        self.top_right.setStyleSheet(u"background-color: rgb(33, 37, 43);")
+        self.top_right.setFrameShape(QFrame.NoFrame)
+        self.top_right.setFrameShadow(QFrame.Raised)
+        self.top_layout.addWidget(self.top_right)
+
+    def bottom(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        self.container_bottom = QFrame(Form)
+        self.container_bottom.setObjectName(u"container_bottom")
+        self.container_bottom.setGeometry(QRect(0, 0, 500, 10))
+        self.container_bottom.setMinimumSize(QSize(0, 10))
+        self.container_bottom.setMaximumSize(QSize(16777215, 10))
+        self.container_bottom.setFrameShape(QFrame.NoFrame)
+        self.container_bottom.setFrameShadow(QFrame.Raised)
+        self.bottom_layout = QHBoxLayout(self.container_bottom)
+        self.bottom_layout.setSpacing(0)
+        self.bottom_layout.setObjectName(u"bottom_layout")
+        self.bottom_layout.setContentsMargins(0, 0, 0, 0)
+        self.bottom_left = QFrame(self.container_bottom)
+        self.bottom_left.setObjectName(u"bottom_left")
+        self.bottom_left.setMinimumSize(QSize(10, 10))
+        self.bottom_left.setMaximumSize(QSize(10, 10))
+        self.bottom_left.setCursor(QCursor(Qt.SizeBDiagCursor))
+        self.bottom_left.setStyleSheet(u"background-color: rgb(33, 37, 43);")
+        self.bottom_left.setFrameShape(QFrame.NoFrame)
+        self.bottom_left.setFrameShadow(QFrame.Raised)
+        self.bottom_layout.addWidget(self.bottom_left)
+        self.bottom = QFrame(self.container_bottom)
+        self.bottom.setObjectName(u"bottom")
+        self.bottom.setCursor(QCursor(Qt.SizeVerCursor))
+        self.bottom.setStyleSheet(u"background-color: rgb(85, 170, 0);")
+        self.bottom.setFrameShape(QFrame.NoFrame)
+        self.bottom.setFrameShadow(QFrame.Raised)
+        self.bottom_layout.addWidget(self.bottom)
+        self.bottom_right = QFrame(self.container_bottom)
+        self.bottom_right.setObjectName(u"bottom_right")
+        self.bottom_right.setMinimumSize(QSize(10, 10))
+        self.bottom_right.setMaximumSize(QSize(10, 10))
+        self.bottom_right.setCursor(QCursor(Qt.SizeFDiagCursor))
+        self.bottom_right.setStyleSheet(u"background-color: rgb(33, 37, 43);")
+        self.bottom_right.setFrameShape(QFrame.NoFrame)
+        self.bottom_right.setFrameShadow(QFrame.Raised)
+        self.bottom_layout.addWidget(self.bottom_right)
+
+    def left(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        self.leftgrip = QFrame(Form)
+        self.leftgrip.setObjectName(u"left")
+        self.leftgrip.setGeometry(QRect(0, 10, 10, 480))
+        self.leftgrip.setMinimumSize(QSize(10, 0))
+        self.leftgrip.setCursor(QCursor(Qt.SizeHorCursor))
+        self.leftgrip.setStyleSheet(u"background-color: rgb(255, 121, 198);")
+        self.leftgrip.setFrameShape(QFrame.NoFrame)
+        self.leftgrip.setFrameShadow(QFrame.Raised)
+
+    def right(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(500, 500)
+        self.rightgrip = QFrame(Form)
+        self.rightgrip.setObjectName(u"right")
+        self.rightgrip.setGeometry(QRect(0, 0, 10, 500))
+        self.rightgrip.setMinimumSize(QSize(10, 0))
+        self.rightgrip.setCursor(QCursor(Qt.SizeHorCursor))
+        self.rightgrip.setStyleSheet(u"background-color: rgb(255, 0, 127);")
+        self.rightgrip.setFrameShape(QFrame.NoFrame)
+        self.rightgrip.setFrameShadow(QFrame.Raised)
 
 def run(conn, t_watch: Threadwatcher, id):
     app = QtWidgets.QApplication(sys.argv)
     
     win = Window(conn, t_watch, id)
     win.setWindowTitle("UiS Subsea")
-    win.showMaximized() # for windows
+    #win.showMaximized() # for windows
     #win.showFullScreen() # for mac
     win.show()
     # win.close()
